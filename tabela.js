93eaaf640f4558fc1165f33a8194a2e2a87b3e97
Igor Gabriel <igjn@outlook.com>
2024-08-20 18:17:18 -0300

    v 3.1

---
+++ tabela.js
@@ -0,0 +1,21 @@
+// Função para editar um documento (exemplo básico)
+/* window.editTarefa = async function(id) {
+  // Aqui você pode implementar uma interface para editar os dados
+  // Por exemplo, mostrar um formulário pré-preenchido
+  alert(`Editar tarefa com ID ${id}. Implemente a função de edição.`);
+  } */
+
+/*   try {
+  await deleteDoc(doc(db, "tarefas", id));
+  console.log(`Documento com ID ${id} apagado com sucesso!`);
+  } catch (error) {
+  console.error("Erro ao apagar documento:", error);
+  }
+  clearTable();
+  populateTable(); */
+  
+  function editarTarefa(id) {
+    console.log("Editar")
+  }
+
+  editarTarefa(0);
